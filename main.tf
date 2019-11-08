b94509bd179c18761aa24644cc91d97b4e6e037e
Maarten van der Hoef <maarten@vanderhoef.eu>
2019-11-08 17:47:59 +0100

    Revert "Upgrade to terraform 0.12"

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # IAM Role for the lambda function
 resource "aws_iam_role" "lambda_ecs_drain_role" {
-  count = local.create_count
+  count = "${var.create}"
   name  = "${var.name}-lambda-ecs-drain-role"
 
   assume_role_policy = <<EOF
@@ -18,21 +18,20 @@ resource "aws_iam_role" "lambda_ecs_drain_role" {
   ]
 }
 EOF
-
 }
 
 # IAM Role AmazonEC2ContainerServiceRole policy attachment
 resource "aws_iam_role_policy_attachment" "amazon_ec2container_service_role" {
-  count      = local.create_count
-  role       = aws_iam_role.lambda_ecs_drain_role[0].id
+  count      = "${var.create}"
+  role       = "${aws_iam_role.lambda_ecs_drain_role.id}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }
 
 # IAM Role policy for the handling of the lifecycle
 resource "aws_iam_role_policy" "lambda_drain_policy" {
-  count = local.create_count
+  count = "${var.create}"
   name  = "${var.name}-lambda-drain-policy"
-  role  = aws_iam_role.lambda_ecs_drain_role[0].name
+  role  = "${aws_iam_role.lambda_ecs_drain_role.name}"
 
   policy = <<EOF
 {
@@ -64,26 +63,21 @@ resource "aws_iam_role_policy" "lambda_drain_policy" {
   ]
 }
 EOF
-
 }
 
 # Publishing the lambda function
 resource "aws_lambda_function" "drain_lambda_function" {
-  count            = local.create_count
+  count            = "${var.create}"
   filename         = "${path.module}/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+  source_code_hash = "${base64sha256(file("${path.module}/lambda.zip"))}"
   function_name    = "${var.name}-lambda-ecs-drain"
-  role             = aws_iam_role.lambda_ecs_drain_role[0].arn
+  role             = "${aws_iam_role.lambda_ecs_drain_role.arn}"
   description      = "${var.name}-lambda-ecs-drain"
   handler          = "index.lambda_handler"
   runtime          = "python2.7"
   timeout          = "300"
 
   lifecycle {
-    ignore_changes = [
-      filename,
-      last_modified,
-    ]
+    ignore_changes = ["filename", "last_modified"]
   }
 }
-
